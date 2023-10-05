08ace09aee4813d8e99119986ce2dd04753cd737
Lucas Andrade <lucasilva108@gmail.com>
2023-10-05 00:02:58 -0300

    refactor: messages

--- Horizon.Domain/Lang/PtBr/PtBrMessages.cs
+++ Horizon.Domain/Lang/PtBr/PtBrMessages.cs
@@ -13,12 +13,12 @@ public abstract class PtBrMessages
     public static string InterServerError { get; } = "Erro interno do servidor";
     public static string BadRequest { get; } = "Verifique se todos os campos estão preenchidos corretamente";
     public static string Error { get; } = "Ocorreu um erro, por favor tente novamente mais tarde";
-    public static string NotFound { get; } = "{0} não encontrado";
+    public static string NotFound { get; } = "{0} não encontrado(a)";
     public static string CreatedFailure { get; } = "Erro ao criar {0}";
     public static string EmailExists { get; set; } = "O e-mail informado já está cadastrado";
 
     // System success messages.
-    public static string CreatedSuccess { get; } = "{0} criado com sucesso";
-    public static string UpdatedSuccess { get; } = "{0} atualizado com sucesso";
-    public static string DeletedSuccess { get; } = "{0} deletado com sucesso";
+    public static string CreatedSuccess { get; } = "{0} criado(a) com sucesso";
+    public static string UpdatedSuccess { get; } = "{0} atualizado(a) com sucesso";
+    public static string DeletedSuccess { get; } = "{0} deletado(a) com sucesso";
 }
