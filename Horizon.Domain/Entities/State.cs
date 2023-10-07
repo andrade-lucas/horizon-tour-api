ad8c4f95c01ec8e7c433d8f1aab1dad72444479f
Lucas Andrade <lucasilva108@gmail.com>
2023-10-07 14:55:03 -0300

    feat: create state entity

---
+++ Horizon.Domain/Entities/State.cs
@@ -0,0 +1,18 @@
+﻿using Horizon.Shared.Entities;
+
+namespace Horizon.Domain.Entities;
+
+public class State : Entity
+{
+    public string Name { get; private set; }
+    public string UF { get; private set; }
+    public Country Country { get; private set; }
+
+    public State(string name, string uf)
+    {
+        Name = name;
+        UF = uf;
+    }
+
+    public void AddCountry(Country country) => Country = country;
+}
