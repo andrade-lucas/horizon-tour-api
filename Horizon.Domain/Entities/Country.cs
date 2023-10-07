f3c1bfed0deaeb2e6b2742efcc5391b14befab84
Lucas Andrade <lucasilva108@gmail.com>
2023-10-07 14:47:57 -0300

    feat: create country entity

---
+++ Horizon.Domain/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using Horizon.Shared.Entities;
+
+namespace Horizon.Domain.Entities
+{
+    public class Country : Entity
+    {
+        public string Name { get; private set; }
+        public string Acronym { get; private set; }
+
+        public Country(string name)
+        {
+            Name = name;
+        }
+
+        public void AddAcronym(string acronym) => Acronym = acronym;
+    }
+}
