93ce597e00337fbe68db54e9fb557319acd50a7f
Lucas Andrade <lucasilva108@gmail.com>
2023-10-07 15:05:05 -0300

    feat: create city entity

---
+++ Horizon.Domain/Entities/City.cs
@@ -0,0 +1,23 @@
+﻿using Horizon.Domain.ValueObjects;
+using Horizon.Shared.Entities;
+
+namespace Horizon.Domain.Entities;
+
+public class City : Entity
+{
+    public string Name { get; private set; }
+    public State State { get; private set; }
+    public string TimeZone { get; private set; }
+    public LatLong LatLong { get; private set; }
+
+    public City(string name)
+    {
+        Name = name;
+    }
+
+    public void AddState(State state) => State = state;
+
+    public void AddTimeZone(string timezone) => TimeZone = timezone;
+
+    public void AddLatLong(LatLong latLong) => LatLong = latLong;
+}
