ab0eb0efeba94a9ca30a10ad27f1d3501d201a4e
Lucas Andrade <lucasilva108@gmail.com>
2023-10-12 14:17:05 -0300

    feat: publish place

--- Horizon.Domain/Entities/Place.cs
+++ Horizon.Domain/Entities/Place.cs
@@ -1,6 +1,7 @@
 ﻿using Horizon.Domain.Enums;
 using Horizon.Domain.ValueObjects;
 using Horizon.Shared.Entities;
+using Horizon.Shared.Messages;
 
 namespace Horizon.Domain.Entities;
 
@@ -23,6 +24,14 @@ public class Place : Entity
         IsOpen = isOpen;
     }
 
+    public Place(string id, string name, EPlaceStatus status, bool isOpen = false, DateTime? createdAt = null, DateTime? updatedAt = null) 
+        : base(id, createdAt, updatedAt)
+    {
+        Name = name;
+        Status = status;
+        IsOpen = isOpen;
+    }
+
     public void AddOwner(User owner) => Owner = owner;
 
     public void AddAddress(Address address) => Address = address;
@@ -34,4 +43,29 @@ public class Place : Entity
     public void AddDescription(string description) => Description = description;
 
     public void AddType(EPlaceType type) => Type = type;
+
+    public bool CanPublish()
+    {
+        bool canPublish = false;
+
+        if (Type != null && Type != EPlaceType.None && Status != EPlaceStatus.Published)
+            canPublish = true;
+
+        return canPublish;
+    }
+
+    public void Publish()
+    {
+        if (Status != EPlaceStatus.Published)
+            Status = EPlaceStatus.Published;
+    }
+
+    public IDictionary<string, string> ErrorsOnPublish()
+    {
+        var errors = new Dictionary<string, string>();
+
+        if (Type == null || Type == EPlaceType.None) errors.Add("Type", string.Format(Messages.InvalidField, "Tipo"));
+
+        return errors;
+    }
 }
--- Horizon.Infra/Repositories/PlaceRepository.cs
+++ Horizon.Infra/Repositories/PlaceRepository.cs
@@ -3,6 +3,7 @@ using Horizon.Domain.Entities;
 using Horizon.Domain.Queries.Inputs;
 using Horizon.Domain.Queries.Responses.Places;
 using Horizon.Domain.Repositories;
+using Horizon.Domain.ValueObjects;
 using Horizon.Infra.Context;
 using Horizon.Shared.Helpers;
 using Horizon.Shared.Outputs;
@@ -18,7 +19,7 @@ public class PlaceRepository : IPlaceRepository
         _db = db;
     }
 
-    public async Task<PaginationResult<GetPlacesResponse>> GetByUser(string userId, QueryPaginate queryPaginate)
+    public async Task<PaginationResult<GetPlacesResponse>> GetByUserAsync(string userId, QueryPaginate queryPaginate)
     {
         var offset = PaginationHelper.GetOffset(queryPaginate.Page, queryPaginate.PageSize);
         var filterSql = queryPaginate.Filter != null
@@ -50,6 +51,51 @@ public class PlaceRepository : IPlaceRepository
         var rows = multi.Read<GetPlacesResponse>().ToList();
 
         return new PaginationResult<GetPlacesResponse>(totalRows, rows.Count, rows);
+    }
+
+    public async Task<Place> GetByIdAsync(string id)
+    {
+        try
+        {
+            var sql = @"
+                SELECT p.*, u.FirstName OwnerFirstName, u.LastName OwnerLastName, u.Email OwnerEmail, c.Name CityName
+                FROM places p
+                LEFT JOIN users u ON u.Id = p.OwnerId
+                LEFT JOIN cities c ON c.Id = p.CityId
+                WHERE p.DeletedAt IS NULL
+                AND p.Id = @id;
+            ";
+
+            var result = await _db.Connection().QueryFirstOrDefaultAsync<GetPlaceByIdResponse>(sql, new { id });
+
+            if (result == null) return null;
+
+            var place = new Place(result.Id, result.Name, result.Status, result.IsOpen, createdAt: result.CreatedAt, updatedAt: result.UpdatedAt);
+            var address = new Address(new City(result.CityId, result.CityName));
+            address.AddStreet(result.Street);
+            address.AddNumber(result.Number);
+            address.AddLatLong(new LatLong(result.Latitude, result.Longitude));
+            address.AddNeighborhood(result.Neighborhood);
+            address.AddZipCode(result.ZipCode);
+            place.AddAddress(address);
+            place.AddType(result.Type);
+            place.AddAutomaticOpen(result.AutomaticOpen);
+            place.AddDescription(result.Description);
+            place.AddPresentationImageUrl(result.PresentationImageUrl);
+            place.AddOwner(
+                new User(result.OwnerId, 
+                new Name(result.OwnerFirstName, result.OwnerLastName, null),
+                new Email(result.OwnerEmail)
+                )
+            );
+
+            return place;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
     }
 
     public async Task CreateAsync(Place place)
@@ -86,6 +132,24 @@ public class PlaceRepository : IPlaceRepository
                 description = place.Description,
                 type = place.Type,
                 createdAt = place.CreatedAt
+            });
+        }
+        catch
+        {
+            throw;
+        }
+    }
+
+    public async Task PublishAsync(Place place)
+    {
+        try
+        {
+            var sql = $"UPDATE places SET Status = @status WHERE Id = @id";
+
+            await _db.Connection().ExecuteAsync(sql, new 
+            { 
+                id = place.Id, 
+                status = place.Status 
             });
         }
         catch
